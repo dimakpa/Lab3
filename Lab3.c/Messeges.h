59e26e38fe3f58be12da45a752e1f994e234459b
Dimaka <70847277+dimakpa@users.noreply.github.com>
2021-10-18 21:03:50 +0300

    Create Messeges.h

---
+++ Lab3.c/Messeges.h
@@ -0,0 +1,15 @@
+
+namespace Lab3_1 {
+
+    void messege_start(class Table *&ep);
+    int D_Add_Exempluar(class Table *&ep);
+    int D_Set_New_Element(class Table *&ep);
+    int D_Print(class Table *&ep);
+    int D_Find_Element(class Table *&ep);
+    int D_Delete_Element_by_Index(class Table *&ep);
+    int D_Delete_Elements(class Table *&ep);
+    int D_Clear_Table(class Table *&ep);
+    int D_Add_Memory(class Table *&ep);
+
+    int dialog(const char *msgs[], int N);
+}
